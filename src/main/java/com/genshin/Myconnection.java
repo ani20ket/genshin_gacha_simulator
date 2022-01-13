8dffc105d69172ea1ec3d8f71dd2fbc604646c7a
ani20ket <aniketdthorat@gmail.com>
2022-01-13 19:41:09 +0530

    Removed ID Pass

--- src/main/java/com/genshin/Myconnection.java
+++ src/main/java/com/genshin/Myconnection.java
@@ -7,7 +7,7 @@ public class Myconnection {
         Connection con = null;
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/genshin_wish","root","Ani123");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/","root","");//Use own database, password
         }catch(Exception ex){
             System.out.println(ex.getMessage());
         }
